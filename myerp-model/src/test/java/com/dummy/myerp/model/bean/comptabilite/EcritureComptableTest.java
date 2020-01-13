56d6469d02d4f49594f72ee74fc300b28f8cff83
MessaoudAbdelatif <dzdevsoft@gmail.com>
2020-01-13 20:03:16 +0100

    fixing 1st bug : REGEX fixing 2nd bug : getTotalCredit was getTotalDebit Implementing getTotalDebit Test using Junit5 !

--- myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
+++ myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
@@ -1,44 +1,70 @@
 package com.dummy.myerp.model.bean.comptabilite;
 
-import java.math.BigDecimal;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.math.BigDecimal;
 import org.apache.commons.lang3.ObjectUtils;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 
 public class EcritureComptableTest {
 
-    private LigneEcritureComptable createLigne(Integer pCompteComptableNumero, String pDebit, String pCredit) {
-        BigDecimal vDebit = pDebit == null ? null : new BigDecimal(pDebit);
-        BigDecimal vCredit = pCredit == null ? null : new BigDecimal(pCredit);
-        String vLibelle = ObjectUtils.defaultIfNull(vDebit, BigDecimal.ZERO)
-                                     .subtract(ObjectUtils.defaultIfNull(vCredit, BigDecimal.ZERO)).toPlainString();
-        LigneEcritureComptable vRetour = new LigneEcritureComptable(new CompteComptable(pCompteComptableNumero),
-                                                                    vLibelle,
-                                                                    vDebit, vCredit);
-        return vRetour;
-    }
+  EcritureComptable vEcriture;
 
-    @Test
-    public void isEquilibree() {
-        EcritureComptable vEcriture;
-        vEcriture = new EcritureComptable();
+  private LigneEcritureComptable createLigne(Integer pCompteComptableNumero, String pDebit,
+      String pCredit) {
+    BigDecimal vDebit = pDebit == null ? null : new BigDecimal(pDebit);
+    BigDecimal vCredit = pCredit == null ? null : new BigDecimal(pCredit);
+    String vLibelle = ObjectUtils.defaultIfNull(vDebit, BigDecimal.ZERO)
+        .subtract(ObjectUtils.defaultIfNull(vCredit, BigDecimal.ZERO)).toPlainString();
+    LigneEcritureComptable vRetour = new LigneEcritureComptable(
+        new CompteComptable(pCompteComptableNumero),
+        vLibelle,
+        vDebit, vCredit);
+    return vRetour;
+  }
 
-        vEcriture.setLibelle("Equilibrée");
-        vEcriture.getListLigneEcriture().add(this.createLigne(1, "200.50", null));
-        vEcriture.getListLigneEcriture().add(this.createLigne(1, "100.50", "33"));
-        vEcriture.getListLigneEcriture().add(this.createLigne(2, null, "301"));
-        vEcriture.getListLigneEcriture().add(this.createLigne(2, "40", "7"));
-        Assert.assertTrue(vEcriture.toString(), vEcriture.isEquilibree());
+  @Before
+  public void start() {
 
-        vEcriture.getListLigneEcriture().clear();
-        vEcriture.setLibelle("Non équilibrée");
-        vEcriture.getListLigneEcriture().add(this.createLigne(1, "10", null));
-        vEcriture.getListLigneEcriture().add(this.createLigne(1, "20", "1"));
-        vEcriture.getListLigneEcriture().add(this.createLigne(2, null, "30"));
-        vEcriture.getListLigneEcriture().add(this.createLigne(2, "1", "2"));
-        Assert.assertFalse(vEcriture.toString(), vEcriture.isEquilibree());
-    }
+    vEcriture = new EcritureComptable();
+  }
 
+  @BeforeEach
+  void setUp() {
+    vEcriture = new EcritureComptable();
+  }
+
+  @Test
+  public void isEquilibree() {
+
+    vEcriture.setLibelle("Equilibrée");
+    vEcriture.getListLigneEcriture().add(this.createLigne(1, "200.50", null));
+    vEcriture.getListLigneEcriture().add(this.createLigne(1, "100.50", "33.00"));
+    vEcriture.getListLigneEcriture().add(this.createLigne(2, null, "200.50"));
+    vEcriture.getListLigneEcriture().add(this.createLigne(2, "33.00", "100.50"));
+    Assert.assertTrue(vEcriture.toString(), vEcriture.isEquilibree());
+
+    vEcriture.getListLigneEcriture().clear();
+    vEcriture.setLibelle("Non équilibrée");
+    vEcriture.getListLigneEcriture().add(this.createLigne(1, "10.00", null));
+    vEcriture.getListLigneEcriture().add(this.createLigne(1, "20.00", "1.00"));
+    vEcriture.getListLigneEcriture().add(this.createLigne(2, null, "20.00"));
+    vEcriture.getListLigneEcriture().add(this.createLigne(2, "1.00", "2.00"));
+    Assert.assertFalse(vEcriture.toString(), vEcriture.isEquilibree());
+  }
+
+  @org.junit.jupiter.api.Test
+  void getTotalDebit() {
+    BigDecimal TROIS_CENT_UN = BigDecimal.valueOf(301);
+    vEcriture.getListLigneEcriture().clear();
+    vEcriture.setLibelle("Test_Debit");
+    vEcriture.getListLigneEcriture().add(this.createLigne(1, "200", null));
+    vEcriture.getListLigneEcriture().add(this.createLigne(1, "101", "33"));
+
+    assertThat(vEcriture.getTotalDebit()).isEqualTo(TROIS_CENT_UN);
+  }
 }
