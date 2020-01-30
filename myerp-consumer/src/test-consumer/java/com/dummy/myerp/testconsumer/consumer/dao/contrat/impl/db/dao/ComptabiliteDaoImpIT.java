7e443343ad741eb8917859198e46c945627d052b
Messaoud <dzdevsoft@gmail.com>
2020-01-30 17:27:27 +0100

    setting environment

---
+++ myerp-consumer/src/test-consumer/java/com/dummy/myerp/testconsumer/consumer/dao/contrat/impl/db/dao/ComptabiliteDaoImpIT.java
@@ -0,0 +1,23 @@
+package com.dummy.myerp.testconsumer.consumer.dao.contrat.impl.db.dao;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.dummy.myerp.consumer.dao.contrat.ComptabiliteDao;
+import com.dummy.myerp.testconsumer.consumer.ConsumerTestCase;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig(locations = {
+    "classpath:com/dummy/myerp/testconsumer/consumer/bootstrapContext.xml"})
+public class ComptabiliteDaoImpIT extends ConsumerTestCase {
+
+  private ComptabiliteDao dao = getDaoProxy().getComptabiliteDao();
+
+
+  @Test
+  void getListCompteComptableTest() {
+    //connected to db-dev & get DATA
+    assertThat(dao.getListCompteComptable().isEmpty()).isFalse();
+  }
+}
