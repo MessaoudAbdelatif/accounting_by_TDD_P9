9d9fbaa93c9c8d91bba82c7a6785dad51977582e
Messaoud <dzdevsoft@gmail.com>
2020-02-17 01:31:51 +0100

    adding some tests

--- myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplNewTest.java
+++ myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplNewTest.java
@@ -1,9 +1,15 @@
 package com.dummy.myerp.business.impl.manager;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyObject;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
 import com.dummy.myerp.business.contrat.BusinessProxy;
 import com.dummy.myerp.business.impl.AbstractBusinessManager;
@@ -14,6 +20,8 @@ import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
 import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
 import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
 import com.dummy.myerp.model.bean.comptabilite.LigneEcritureComptable;
+import com.dummy.myerp.technical.exception.FunctionalException;
+import com.dummy.myerp.technical.exception.NotFoundException;
 import java.math.BigDecimal;
 import java.util.Date;
 import org.junit.jupiter.api.BeforeEach;
@@ -46,8 +54,6 @@ public class ComptabiliteManagerImplNewTest extends AbstractBusinessManager {
   @BeforeEach
   public void setUp() {
     this.vEcritureComptable = new EcritureComptable();
-    AbstractBusinessManager.configure(businessProxy, daoProxy, transactionManager);
-    given(getDaoProxy().getComptabiliteDao()).willReturn(this.comptabiliteDao);
 
     vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));
     vEcritureComptable.setDate(new Date());
@@ -60,26 +66,39 @@ public class ComptabiliteManagerImplNewTest extends AbstractBusinessManager {
         new BigDecimal(123)));
   }
 
+  private void initClassUnderTestDaoMock() {
+    AbstractBusinessManager.configure(businessProxy, daoProxy, transactionManager);
+    given(getDaoProxy().getComptabiliteDao()).willReturn(this.comptabiliteDao);
+  }
+
   @Test
   void getListCompteComptable() {
+    initClassUnderTestDaoMock();
+
     classUnderTest.getListCompteComptable();
     then(getDaoProxy()).should(times(1)).getComptabiliteDao();
   }
 
   @Test
   void getListJournalComptable() {
+    initClassUnderTestDaoMock();
+
     classUnderTest.getListJournalComptable();
     then(getDaoProxy()).should(times(1)).getComptabiliteDao();
   }
 
   @Test
   void getListEcritureComptable() {
+    initClassUnderTestDaoMock();
+
     classUnderTest.getListEcritureComptable();
     then(getDaoProxy()).should(times(1)).getComptabiliteDao();
   }
 
   @Test
   void getSequenceEcritureComptables() {
+    initClassUnderTestDaoMock();
+
     classUnderTest.getSequenceEcritureComptables(2019);
     then(getDaoProxy()).should(times(1)).getComptabiliteDao();
 
@@ -87,17 +106,45 @@ public class ComptabiliteManagerImplNewTest extends AbstractBusinessManager {
 
   @Test
   public void addReference() {
+    initClassUnderTestDaoMock();
 
     classUnderTest.addReference(vEcritureComptable);
     then(getDaoProxy()).should(atLeast(3)).getComptabiliteDao();
+  }
 
+  @Test
+  final void checkEcritureComptable() {
+    //Add new LigneEcritureComptable that make our vEcritureComptable unbalance and triggers a FunctionalException.
+    this.vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable());
+    //Testing the checkEcritureComptable method.
+    FunctionalException functionalException = assertThrows(FunctionalException.class,
+        () -> classUnderTest.checkEcritureComptable(vEcritureComptable));
+    //Checking the right FunctionalExceptionMessage.
+    assertThat(functionalException.getMessage()).isEqualTo("L'écriture comptable ne respecte pas les règles de gestion.");
   }
 
 
-//  @Test
-//  public void checkEcritureComptableUnit() {
-//  }
-//
+  @Test
+  public void checkEcritureComptableUnit_regexPatternFail_triggedFunctionalException(){
+
+    vEcritureComptable.setReference("KSBCKQSBC");
+
+    FunctionalException functionalException = assertThrows(FunctionalException.class,
+        () -> classUnderTest.checkEcritureComptable(vEcritureComptable));
+    assertThat(functionalException.getMessage()).isEqualTo("L'écriture comptable ne respecte pas les règles de gestion.");
+
+  }
+  @Test
+  public void checkEcritureComptableUnit_labelleSizeFail_triggedFunctionalException(){
+
+    vEcritureComptable.setLibelle("");
+
+    FunctionalException functionalException = assertThrows(FunctionalException.class,
+        () -> classUnderTest.checkEcritureComptable(vEcritureComptable));
+    assertThat(functionalException.getMessage()).isEqualTo("L'écriture comptable ne respecte pas les règles de gestion.");
+
+  }
+
 //  @Test
 //  public void checkEcritureComptableContext() {
 //  }
