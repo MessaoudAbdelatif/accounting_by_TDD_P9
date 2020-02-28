b9d82789d10b313ef9d993e447b265693de783f5
Messaoud <dzdevsoft@gmail.com>
2020-02-28 13:01:04 +0100

    add model test

--- myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/JournalComptableTest.java
+++ myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/JournalComptableTest.java
@@ -2,6 +2,7 @@ package com.dummy.myerp.model.bean.comptabilite;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class JournalComptableTest {
@@ -9,11 +10,21 @@ public class JournalComptableTest {
   private String code = "fakeCode";
   private String libelle = "fakeLibelle";
 
+  @BeforeEach
+  public void setUp(){
+    this.journalComptable = new JournalComptable(code,libelle);
+  }
+
   @Test
   public void toStringTest(){
-    this.journalComptable = new JournalComptable(code,libelle);
     String expectedString = "JournalComptable{code='"+code+"', libelle='"+libelle+"'}";
     String resultString = this.journalComptable.toString();
     assertThat(expectedString).isEqualTo(resultString);
+  }
+
+  @Test
+  public void Constructor_withDefaultValues(){
+    assertThat(journalComptable.getLibelle()).isEqualTo(libelle);
+    assertThat(journalComptable.getCode()).isEqualTo(code);
   }
 }
